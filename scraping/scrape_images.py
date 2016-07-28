c73e0f4fc7c826b6787f2e4b5eb86b43d4487183
Saurav Das <sauradas@cisco.com>
2016-07-28 11:20:45 +0530

    Setting gitignore

--- scraping/scrape_images.py
+++ scraping/scrape_images.py
@@ -1,21 +1,30 @@
+import requests
 import sys; sys.path.insert(0, '../dataset/')
 
 from data_manipulator import DataManager
 
 class ImageScraper:
 	def __init__(self):
-		pass
+		self.dataManagerObj = DataManager()
 
 	def getAllPicturesFromWeb(self):
-		obj = DataManager()
 		while True:
-			line = obj.getMovieNamesOneByOne()
+			line = self.dataManagerObj.getMovieNamesOneByOne()
 			if not line:
 				break
 			elif line == 'skipped':
 				continue
 			else:
-				print line
+				# Parse the line to get the required info
+				# Send the request to OMDb
+				r = requests.get('')
+				# If response is False from OMDb, try other combinations.
+				# Name may be in the follwing way - Seven (Se7en). Try with name in (...)
+				# Name may be Usual Suspects, The. Try putting things after the comma before.
+				# Remove the punctuation marks and just send the whole thing 
+				# If all combinations fail, just add to the failed list. We will know what failed atleast.
+				
+				
 
 obj = ImageScraper()
 obj.getAllPicturesFromWeb()
